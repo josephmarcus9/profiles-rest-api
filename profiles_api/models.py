018fd5e8151319d90710ed610251091450233585
Joseph <joseph@businessoptics.co.za>
2020-11-14 22:36:24 +0200

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address to proceed')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password) #encrypts the set_password
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) #allows us to see if the user's account is activate
+    is_staff = models.BooleanField(default= False) #allows us to determine if they should have access to the django administrative
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email' #overwrites username field --> instead whenever users login they will just enter there email address
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrive full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrive short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of the user"""
+        return self.email
